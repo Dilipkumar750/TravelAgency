c7468b57f4601a6eab685d21359ba0fb33744d65
Dilipkumar750 <dilipk5406@gmail.com>
2025-01-10 09:52:41 +0530

    dilip changes

--- src/Components/StayComponent/Honeymoon.jsx
+++ src/Components/StayComponent/Honeymoon.jsx
@@ -13,58 +13,66 @@ import beach4 from "../../assets/honey4.png";
 import beach5 from "../../assets/honey5.jpeg";
 
 const cards = [
-    {
-      id: 1,
-      title: "Santorini Escape",
-      price: "₹1,10,000",
-      rating: "4.9",
-      duration: "6 Days 5 Nights",
-      description: "Private sunset cruise, luxury accommodations, scenic views",
-      image: beach1,
-      features: { flights: 2, hotels: 1, transfers: 2, activities: 4 },
-    },
-    {
-      id: 2,
-      title: "Parisian Romance",
-      price: "₹1,20,000",
-      rating: "4.9",
-      duration: "5 Days 4 Nights",
-      description: "Romantic city tours, Eiffel Tower views, luxury dining",
-      image: beach2,
-      features: { flights: 2, hotels: 1, transfers: 2, activities: 3 },
-    },
-    {
-      id: 3,
-      title: "Swiss Alps Retreat",
-      price: "₹1,15,000",
-      rating: "4.8",
-      duration: "5 Days 4 Nights",
-      description: "Snow adventures, scenic mountain views, luxury stays",
-      image: beach3,
-      features: { flights: 2, hotels: 1, transfers: 2, activities: 5 },
-    },
-    {
-      id: 4,
-      title: "Kerala Backwaters",
-      price: "₹88,500",
-      rating: "4.7",
-      duration: "4 Days 3 Nights",
-      description: "Scenic backwater cruises, traditional houseboats, cultural tours",
-      image: beach5,
-      features: { flights: 2, hotels: 1, transfers: 2, activities: 4 },
-    },
-    {
-      id: 5,
-      title: "Andaman Adventure",
-      price: "₹1,00,000",
-      rating: "4.8",
-      duration: "7 Days 6 Nights",
-      description: "Beach exploration, snorkeling, crystal clear waters",
-      image: beach4,
-      features: { flights: 2, hotels: 1, transfers: 2, activities: 5 },
-    },
-  ];
-  
+  {
+    id: 1,
+    title: "Santorini Escape",
+    originalPrice: 120000, 
+    discountPrice: 110000,
+    rating: "4.9",
+    duration: "6 Days 5 Nights",
+    description: "Private sunset cruise, luxury accommodations, scenic views",
+    image: beach1,
+    features: { flights: 2, hotels: 1, transfers: 2, activities: 4 },
+  },
+  {
+    id: 2,
+    title: "Parisian Romance",
+    originalPrice: 130000,
+    discountPrice: 120000,
+    rating: "4.9",
+    duration: "5 Days 4 Nights",
+    description: "Romantic city tours, Eiffel Tower views, luxury dining",
+    image: beach2,
+    features: { flights: 2, hotels: 1, transfers: 2, activities: 3 },
+  },
+  {
+    id: 3,
+    title: "Swiss Alps Retreat",
+    originalPrice: 125000,
+    discountPrice: 115000,
+    rating: "4.8",
+    duration: "5 Days 4 Nights",
+    description: "Snow adventures, scenic mountain views, luxury stays",
+    image: beach3,
+    features: { flights: 2, hotels: 1, transfers: 2, activities: 5 },
+  },
+  {
+    id: 4,
+    title: "Kerala Backwaters",
+    originalPrice: 95000,
+    discountPrice: 88500,
+    rating: "4.7",
+    duration: "4 Days 3 Nights",
+    description: "Scenic backwater cruises, traditional houseboats, cultural tours",
+    image: beach5,
+    features: { flights: 2, hotels: 1, transfers: 2, activities: 4 },
+  },
+  {
+    id: 5,
+    title: "Andaman Adventure",
+    originalPrice: 110000,
+    discountPrice: 100000,
+    rating: "4.8",
+    duration: "7 Days 6 Nights",
+    description: "Beach exploration, snorkeling, crystal clear waters",
+    image: beach4,
+    features: { flights: 2, hotels: 1, transfers: 2, activities: 5 },
+  },
+];
+
+const calculateDiscountPercentage = (originalPrice, discountPrice) => {
+  return Math.round(((originalPrice - discountPrice) / originalPrice) * 100);
+};
 
 const Honeymoon = () => {
   const swiperRef = useRef(null);
@@ -96,53 +104,74 @@ const Honeymoon = () => {
         }}
         className="rounded-xl"
       >
-        {cards.map((card) => (
-          <SwiperSlide key={card.id}>
-            <div
-              className="max-w-sm mx-auto rounded-md overflow-hidden shadow-md hover:shadow-lg cursor-pointer"
-              onClick={handleCardClick}
-            >
-              {/* Card Image */}
-              <div className="relative">
-                <img className="w-full h-48 object-cover" src={card.image} alt={card.title} />
-                <div className="absolute top-0 right-0 bg-red-500 text-white px-2 py-1 rounded-md text-sm font-medium">
-                  {card.rating} ★
+        {cards.map((card) => {
+          const discountPercentage = calculateDiscountPercentage(card.originalPrice, card.discountPrice);
+          return (
+            <SwiperSlide key={card.id}>
+              <div
+                className="max-w-sm mx-auto rounded-md overflow-hidden shadow-md hover:shadow-lg cursor-pointer"
+                onClick={handleCardClick}
+              >
+                <div className="relative">
+                  <div className="absolute top-0 right-0 bg-red-500 text-white px-2 py-1 rounded-md text-sm font-medium">
+                    {card.rating} ★
+                  </div>
+                  <div
+                    className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-black via-transparent to-transparent"
+                  />
+                </div>  
+
+                <div
+                  className="p-4 bg-white relative z-10"
+                  style={{
+                    backgroundImage: `url(${card.image})`,
+                    backgroundPosition: 'center bottom', 
+                    backgroundSize: 'cover', 
+                    backgroundRepeat: 'no-repeat',
+                  }}
+                >
+                  <h3 className="text-lg font-medium mb-2 text-white">{card.title}</h3>
+                  <p className="text-white text-sm mb-2">{card.duration}</p>
+                  <p className="text-white text-xs mb-4">{card.description}</p>
+
+                  {/* Features */}
+                  <div className="grid grid-cols-2 gap-2 mb-4 text-white text-sm">
+                    <div className="flex items-center">
+                      <FaPlane className="text-blue-500 mr-2" /> {card.features.flights} Flights
+                    </div>
+                    <div className="flex items-center">
+                      <FaHotel className="text-blue-500 mr-2" /> {card.features.hotels} Hotels
+                    </div>
+                    <div className="flex items-center">
+                      <FaCar className="text-blue-500 mr-2" /> {card.features.transfers} Transfers
+                    </div>
+                    <div className="flex items-center">
+                      <FaUmbrellaBeach className="text-blue-500 mr-2" /> {card.features.activities} Activities
+                    </div>
+                  </div>
+
+                  <div className="flex">
+                    <div className="items-center justify-between mb-2">
+                      <span className="line-through text-gray-500">{`₹${card.originalPrice.toLocaleString()}`}</span>
+                      <span className="font-bold text-lg text-blue-500">{`₹${card.discountPrice.toLocaleString()}`}</span>
+
+                      <div className="text-xs text-red-500 font-medium mb-4">
+                        Save {discountPercentage}% Off
+                      </div>
+                    </div>
+                    <div className="flex items-center justify-between mx-auto">
+                      <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+                        Book Now
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
 
-              {/* Card Content */}
-              <div className="p-4 bg-white">
-                <h3 className="text-lg font-medium mb-2">{card.title}</h3>
-                <p className="text-gray-600 text-sm mb-2">{card.duration}</p>
-                <p className="text-gray-600 text-xs mb-4">{card.description}</p>
 
-                {/* Features */}
-                <div className="grid grid-cols-2 gap-2 mb-4 text-gray-600 text-sm">
-                  <div className="flex items-center">
-                    <FaPlane className="text-blue-500 mr-2" /> {card.features.flights} Flights
-                  </div>
-                  <div className="flex items-center">
-                    <FaHotel className="text-blue-500 mr-2" /> {card.features.hotels} Hotels
-                  </div>
-                  <div className="flex items-center">
-                    <FaCar className="text-blue-500 mr-2" /> {card.features.transfers} Transfers
-                  </div>
-                  <div className="flex items-center">
-                    <FaUmbrellaBeach className="text-blue-500 mr-2" /> {card.features.activities} Activities
-                  </div>
-                </div>
-
-                {/* Price and Button */}
-                <div className="flex items-center justify-between">
-                  <span className="font-bold text-lg text-blue-500">{card.price}</span>
-                  <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-                    Book Now
-                  </button>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </div>
   );
